5f99333ebe9c7071617fce5a5d9218c559713155
Ravijoshi28 <156401274+Ravijoshi28@users.noreply.github.com>
2025-08-24 10:34:51 +0530

    complete file

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,23 +1,24 @@
 import { User } from "../database/user.model.js";
 
-export const callback=async (req,res,next)=>{
-
-    try {
-        const {id,firstName,lastName,imageUrl}=req.body;
-        const user=await User.findOneAndUpdate({clerkId:id});
-
-        if(!user) {
-            await User.create({
-                clerkId:id,
-                fullName:`${firstName} ${lastName}`,
-                imageUrl
-            });
-        } 
-        res.status(200).json({message:"User has been registered..."});
+export const callback = async (req, res, next) => {
+  try {
+    const { id, firstName, lastName, imageUrl } = req.body;
 
 
-    } catch (error) {
-        console.log("error at the callback function in auth controller",error);
-      next(error);
-    }
-}
+    await User.updateOne(
+      { clerkId: id },          
+      {
+        $setOnInsert: {
+          fullName: `${firstName} ${lastName}`,
+          imageUrl
+        }
+      },
+      { upsert: true }           
+    );
+
+    res.status(200).json({ message: "User has been registered..." });
+  } catch (error) {
+    console.log("Error at the callback function in auth controller", error);
+    next(error);
+  }
+};
